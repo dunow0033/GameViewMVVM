6e3c45209e876f195b51f5bf76d80b4ca1872168
Brian Donnoe <dunow33@hotmail.com>
2022-07-01 01:05:02 -0400

    I think it's finished

--- app/src/main/java/com/express/android/gameviewmvvm/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/express/android/gameviewmvvm/screens/score/ScoreFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.express.android.gameviewmvvm.R
 import com.express.android.gameviewmvvm.databinding.ScoreFragmentBinding
@@ -33,11 +35,24 @@ class ScoreFragment : Fragment() {
             false
         )
 
-        viewModelFactory = ScoreViewModelFactory(ScoreFragmentArgs.fromBundle(arguments!!).score)
+        viewModelFactory = ScoreViewModelFactory(ScoreFragmentArgs.fromBundle(requireArguments()).score)
         viewModel = ViewModelProvider(this, viewModelFactory)
             .get(ScoreViewModel::class.java)
 
-        binding.scoreText.text = viewModel.score.toString()
+        viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
+            binding.scoreText.text = newScore.toString()
+        })
+
+        viewModel.eventPlayAgain.observe(viewLifecycleOwner, Observer { playAgain ->
+            if(playAgain) {
+                findNavController().navigate(ScoreFragmentDirections.actionRestart())
+                viewModel.onPlayAgainComplete()
+            }
+        })
+
+        binding.playAgainButton.setOnClickListener {
+            viewModel.onPlayAgain()
+        }
 
         return binding.root
     }
